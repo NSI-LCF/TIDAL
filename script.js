378bdf9ed78f429020f515116a71768ffb548765
GuiSamSamTei <95433072+GuiSamSamTei@users.noreply.github.com>
2021-12-14 16:07:41 +0100

    Update script.js

--- script.js
+++ script.js
@@ -4,17 +4,7 @@ setInterval(showTime, 1000);
             let hour = time.getHours();
             let min = time.getMinutes();
             let sec = time.getSeconds();
-            am_pm = "AM";
-  
-            if (hour > 12) {
-                hour -= 12;
-                am_pm = "PM";
-            }
-            if (hour == 0) {
-                hr = 12;
-                am_pm = "AM";
-            }
-  
+
             hour = hour < 10 ? "0" + hour : hour;
             min = min < 10 ? "0" + min : min;
             sec = sec < 10 ? "0" + sec : sec;
@@ -30,7 +20,7 @@ setInterval(showTime, 1000);
             let mois=Mois[time.getMonth()]
 
             let currentTime = day + " " + date + " " + mois + " " + hour + ":" 
-                + min + ":" + sec + am_pm;
+                + min + ":" + sec;
   
             document.getElementById("date")
                 .innerHTML = currentTime;
