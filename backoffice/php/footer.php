5f03e224cbf160e5ce52869460b71a3dccfb8f78
ElGammaYT <aidanbalap@gmail.com>
2021-12-20 10:50:08 +0100

    TO DO
    Copied template + Funcitons to do

---
+++ backoffice/php/footer.php
@@ -0,0 +1,13 @@
+<?php
+function ShowFooter() {
+    return '<footer class="tm-footer row tm-mt-small">
+    <div class="col-12 font-weight-light">
+        <p class="text-center text-white mb-0 px-4 small">
+            Copyright &copy; <b>2021</b> All rights reserved. 
+            
+            Design: TIDAL
+        </p>
+    </div>
+</footer>';
+}
+?>
