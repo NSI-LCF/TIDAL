a656e0ef3cb5deaca148ba46de05c865d80d021e
Bleu <65407864+Bleubg@users.noreply.github.com>
2022-02-07 11:21:17 +0100

    ip update db operational

--- backoffice/php/conf.php
+++ backoffice/php/conf.php
@@ -1,5 +1,5 @@
 <?php     
-        $dsn = 'mysql:host=109.111.109.101:443;dbname=tidal;charset=UTF8';
+        $dsn = 'mysql:host=91.187.82.220:443;dbname=tidal;charset=UTF8';
         $username = 'tidal';
         $password = 'TIDAL-db-2022';
         $dbh = new PDO($dsn, $username, $password) or die("Pb de connexion !");
